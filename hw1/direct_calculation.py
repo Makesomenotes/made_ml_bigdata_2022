849db0b14e4b53c31e9b287a83d650880b39f1b0
Maksim Gritskikh <gritskih.maksim@gmail.com>
2022-10-26 23:35:40 +0300

    add hw1

---
+++ hw1/direct_calculation.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('/Users/maksimgritskikh/MADE/made_ml_bigdata_2022/hw1/AB_NYC_2019.csv')
+price = df['price'].dropna()
+print(np.mean(price), np.std(price) ** 2)
