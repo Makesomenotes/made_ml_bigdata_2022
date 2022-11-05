0cfa507d09ace3ef70087d504143ec77543265ba
Maksim Gritskikh <gritskih.maksim@gmail.com>
2022-11-06 00:05:30 +0300

    add small changes

--- hw3_1/src/main/scala/Main.scala
+++ hw3_1/src/main/scala/Main.scala
@@ -7,25 +7,26 @@ import java.io.File
 object Main {
   def main(args: Array[String]): Unit = {
     println("start")
-    val data_path = "/Users/maksimgritskikh/MADE/made_ml_bigdata_2022/hw3_1/data/diabetes_data.csv"
-    val target_path = "/Users/maksimgritskikh/MADE/made_ml_bigdata_2022/hw3_1/data/diabetes_target.csv"
+    val data_path = "/Users/maksimgritskikh/MADE/made_ml_bigdata_2022/hw3_1/data/features.csv"
+    val target_path = "/Users/maksimgritskikh/MADE/made_ml_bigdata_2022/hw3_1/data/target.csv"
     val out_path = "/Users/maksimgritskikh/MADE/made_ml_bigdata_2022/hw3_1/data/pred.csv"
 
     val model = new LinRegReg
     val data = csvread(file=new File(data_path), separator=',')
     val target = csvread(file=new File(target_path), separator=',').toDenseVector
     val n = target.length
-    val m = data.cols
     val train_part = (n * 0.8).toInt
-    println((n, train_part))
-    val (yTrain, yTest) = (target(0 until train_part), target(train_part until n))
-    val (xTrain, xTest) = (data(0 until train_part, ::).toDenseMatrix, data(train_part until n, ::).toDenseMatrix)
+
+    val (y_train, y_test) = (target(0 until train_part), target(train_part until n))
+    val (x_train, x_test) = (data(0 until train_part, ::).toDenseMatrix, data(train_part until n, ::).toDenseMatrix)
     println(s"Data processed")
-    val (weights, bias) = model.fit(xTrain, yTrain, 1000, 0.01)
+
+    val (weights, bias) = model.fit(x_train, y_train, 1000, 0.01)
     println(s"Model fitted")
-    val y_pred = model.predict(xTest, weights, bias)
+
+    val y_pred = model.predict(x_test, weights, bias)
     csvwrite(new File(out_path), separator = ',', mat = y_pred.toDenseMatrix)
-    println(s"Got predictions. RMSE on validation dataset: ${model.RMSE(yTest, y_pred)}")
+    println(s"Got predictions. RMSE on validation dataset: ${model.RMSE(y_test, y_pred)}")
     println(s"All done. Predictions are saved to: ${out_path}")
   }
 }
--- hw3_1/src/main/scala/utility/LinReg.scala
+++ hw3_1/src/main/scala/utility/LinReg.scala
@@ -4,16 +4,14 @@ import breeze.linalg.{DenseMatrix, DenseVector, sum}
 import breeze.numerics.{pow, sqrt}
 
 class LinRegReg {
-  // implementation of linear regression using gradient descent
-
   def fit(X: DenseMatrix[Double], y: DenseVector[Double],
-          nIter: Int, learnRate: Double): (DenseVector[Double], Double) = {
+          n: Int, learnRate: Double): (DenseVector[Double], Double) = {
     var (weights, bias) = (DenseVector.zeros[Double](X.cols), .0)
-    for (_ <- 0 to nIter) {
-      val yHat = (X * weights) + bias
-      weights :-= learnRate * 2 * (X.t * (yHat - y))
+    for (_ <- 0 to n) {
+      val y_hat = (X * weights) + bias
+      weights :-= learnRate * 2 * (X.t * (y_hat - y))
       weights = weights.map(el => el / X.rows)
-      bias -= learnRate * 2 * sum(yHat - y) / X.rows
+      bias -= learnRate * 2 * sum(y_hat - y) / X.rows
     }
     (weights, bias)
   }
@@ -24,8 +22,8 @@ class LinRegReg {
     res
   }
 
-  def RMSE(yTrue: DenseVector[Double], yPred: DenseVector[Double]): Double = {
-    val error = sum((yTrue - yPred).map(el => pow(el, 2))) / yTrue.length
+  def RMSE(y_true: DenseVector[Double], y_pred: DenseVector[Double]): Double = {
+    val error = sum((y_true - y_pred).map(el => pow(el, 2))) / y_true.length
     sqrt(error)
   }
 }
